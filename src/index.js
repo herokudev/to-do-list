a4c7ead35215c834e720479226646831ee3830fe
herokudev <ao43085@gmail.com>
2021-08-25 11:17:12 -0600

    Basic list code changes
    Basic list code changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
+/*function component1() {
+  const element = document.createElement('div');
+  const icon = document.createElement('i');
+
+  // Lodash, now imported by this script
+  element.innerHTML = "Today's To Do";
+  element.classList.add('list-header');
+
+  return element;
+}*/
+
+function component2() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.innerHTML = "Add to your list...";
+  element.classList.add('list-item');
 
   return element;
 }
 
-document.body.appendChild(component());
+function component3() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = "Clear all completed";
+  element.classList.add('list-footer');
+
+  return element;
+}
+
+//document.body.appendChild(component1());
+document.body.appendChild(component2());
+document.body.appendChild(component3());
