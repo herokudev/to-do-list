124fbe86ee86b003a170c70966a304bd4853c8f0
herokudev <ao43085@gmail.com>
2021-08-25 11:43:07 -0600

    Readme documentation
    Readme documentation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import _ from 'lodash';
 import './style.css';
-
-/*function component1() {
-  const element = document.createElement('div');
-  const icon = document.createElement('i');
-
-  // Lodash, now imported by this script
-  element.innerHTML = "Today's To Do";
-  element.classList.add('list-header');
-
-  return element;
-}*/
 
 function component2() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = "Add to your list...";
+  element.innerHTML = 'Add to your list...';
   element.classList.add('list-item');
 
   return element;
@@ -26,12 +14,11 @@ function component3() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = "Clear all completed";
+  element.innerHTML = 'Clear all completed';
   element.classList.add('list-footer');
 
   return element;
 }
 
-//document.body.appendChild(component1());
 document.body.appendChild(component2());
 document.body.appendChild(component3());
